5cd6e7342a2de078b8682a4732daaa8f8fa3be74
Eugenio di Lorenzo <gege92@teletu.it>
2017-06-07 15:12:03 -0700

    Initial Commit

---
+++ test_WWDC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_WWDC
+//
+//  Created by Eugenio di Lorenzo on 07/06/2017.
+//  Copyright © 2017 Eugenio di Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
